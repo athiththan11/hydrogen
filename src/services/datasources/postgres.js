9aa8bd48022cb8315638a97cc0b4ce02598ef14f
athiththan11 <code19111995@gmail.com>
2019-08-05 12:04:32 +0530

    identity xml implementation

--- src/services/datasources/postgres.js
+++ src/services/datasources/postgres.js
@@ -4,12 +4,23 @@ const prettifyXml = require('prettify-xml');
 
 const { logger } = require('../../utils/logger');
 
+let _t = '\t\t';
+let _n = '\n\n';
+
 let pMasterDatasource =
 	process.cwd() + '/repository/conf/datasources/master-datasources.xml';
+let pIdentity = process.cwd() + '/repository/conf/identity/identity.xml';
 
 exports.configPostgres = async function (log, cli) {
 	await parseMasterDatasource(log).then(data => {
 		alterMasterDatasource(log, data).then(() => {
+			cli.action.stop();
+		});
+	});
+
+	cli.action.start('\taltering master-datasources.xml');
+	await parseIdentity(log).then(data => {
+		alterIdentity(log, data).then(() => {
 			cli.action.stop();
 		});
 	});
@@ -52,10 +63,10 @@ async function alterMasterDatasource(log, data) {
 			0,
 			config.lastIndexOf('<datasource><name>WSO2_POSTGRES')
 		) +
-		'\n\n\t\t<!-- HYDROGENERATED: postgres datasource added -->\n\t\t' +
+		`${_n}${_t}<!-- HYDROGENERATED: postgres datasource added -->\n${_t}` +
 		prettifyXml(postgres, { indent: 4, newline: '\n' }).replace(
 			/\n/g,
-			'\n\t\t'
+			`\n${_t}`
 		) +
 		'\n' +
 		leftOver;
@@ -101,6 +112,52 @@ function buildPostgresDatasource(doc) {
 		.node('validationInterval', '30000')
 		.parent()
 		.node('defaultAutoCommit', 'true');
+
+	return postgresElement;
+}
+
+// #endregion
+
+// #region identity
+
+async function parseIdentity(log) {
+	let jsonData = fs.readFileSync(pIdentity, 'utf8');
+	jsonData = libxmljs.parseXml(jsonData);
+
+	return jsonData;
+}
+
+async function alterIdentity(log, data) {
+	let doc = new libxmljs.Document(data);
+	let postgresElement = buildIdentity(doc);
+
+	data.root()
+		.get('//*[local-name()="JDBCPersistenceManager"]/*[local-name()="DataSource"]/*[local-name()="Name"]')
+		.replace(postgresElement);
+
+	let config = data.toString();
+
+	// extract postgres config
+	let altered =
+		config.substring(
+			0,
+			config.lastIndexOf('<Name>jdbc/WSO2PostgresCarbonDB')
+		) +
+		`\n${_t}\t<!-- HYDROGENERATED: postgres datasource added. changed jdbc/WSO2CarbonDB -->\n${_t}\t` +
+		config.substring(
+			config.lastIndexOf('<Name>jdbc/WSO2PostgresCarbonDB')
+		) +
+		'\n\n';
+
+	fs.writeFileSync(pIdentity, altered, 'utf8');
+}
+
+function buildIdentity(doc) {
+	let postgresElement = new libxmljs.Element(
+		doc,
+		'Name',
+		'jdbc/WSO2PostgresCarbonDB'
+	);
 
 	return postgresElement;
 }
