1a21d828e3a7b9754bb858bf296da3b0bd0ab1e3
athiththan11 <code19111995@gmail.com>
2019-08-05 20:48:25 +0530

    oracle extended

---
+++ src/services/datasources/oracle.js
@@ -0,0 +1,11 @@
+const { configureDatasource } = require('./generic');
+
+let args = {};
+args._ = 'Oracle';
+args._connectionUrl = 'jdbc:oracle:thin:@localhost:1521/orcl';
+args._driver = 'oracle.jdbc.OracleDriver';
+args._validationQuery = 'SELECT 1 FROM DUAL';
+
+exports.configure = async function (log, cli) {
+	configureDatasource(log, cli, args);
+};
