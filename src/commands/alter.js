59fcaac4afeb8213af83f0591e7c7dfb76e3e733
athiththan11 <code19111995@gmail.com>
2019-08-05 14:31:14 +0530

    mysql implementation

--- src/commands/alter.js
+++ src/commands/alter.js
@@ -3,6 +3,7 @@ const { cli } = require('cli-ux');
 const { logger } = require('../utils/logger');
 
 const Postgres = require('../services/datasources/postgres');
+const MySQL = require('../services/datasources/mysql');
 
 class AlterCommand extends Command {
 	async run() {
@@ -11,10 +12,13 @@ class AlterCommand extends Command {
 		const version = flags.version;
 		const datasource = flags.datasource;
 
-		this.log(`altering ${product}-${version} with ${datasource} configurations`);
+		this.log(`starting to alter ${product}-${version} with ${datasource} configurations`);
 
 		cli.action.start('\taltering master-datasources.xml');
-		await Postgres.configPostgres(this.log, cli);
+		if (datasource === 'postgres')
+			await Postgres.configPostgres(this.log, cli);
+		else if (datasource === 'mysql')
+			await MySQL.configMySQL(this.log, cli);
 	}
 }
 
@@ -47,8 +51,8 @@ AlterCommand.flags = {
 		hidden: false,
 		multiple: false,
 		required: true,
-		default: 'postgres',
-		options: ['postgres'],
+		default: 'mysql',
+		options: ['postgres', 'mysql'],
 	}),
 };
 
