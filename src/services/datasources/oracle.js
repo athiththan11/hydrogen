b35a7fec8e8ff2229e6d952c8bf0384a810877d7
athiththan11 <code19111995@gmail.com>
2019-08-12 00:54:38 +0530

    refactored

--- src/services/datasources/oracle.js
+++ src/services/datasources/oracle.js
@@ -1,11 +1,12 @@
 const { configureDatasource } = require('./generic');
 
 let args = {};
-args._ = 'Oracle';
+args._ = 'OracleCarbonDB';
+args._name = 'Oracle_Carbon_DB';
 args._connectionUrl = 'jdbc:oracle:thin:@localhost:1521/orcl';
 args._driver = 'oracle.jdbc.OracleDriver';
 args._validationQuery = 'SELECT 1 FROM DUAL';
 
-exports.configure = async function (ocli) {
-	configureDatasource(ocli, args);
+exports.configure = async function (ocli, product) {
+	configureDatasource(ocli, args, product);
 };
--- src/services/datasources/postgres.js
+++ src/services/datasources/postgres.js
@@ -1,12 +1,13 @@
 const { configureDatasource } = require('./generic');
 
 let args = {};
-args._ = 'Postgres';
+args._ = 'PostgresCarbonDB';
+args._name = 'Postgres_Carbon_DB';
 args._connectionUrl = 'jdbc:postgresql://localhost:5432/wso2postgres';
 args._defaultAutoCommit = 'true';
 args._driver = 'org.postgresql.Driver';
 args._validationQuery = 'SELECT 1; COMMIT';
 
-exports.configure = async function (ocli) {
-	configureDatasource(ocli, args);
+exports.configure = async function (ocli, product) {
+	configureDatasource(ocli, args, product);
 };
