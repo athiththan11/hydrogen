c3d95d1c135ec21ef9c29e17dbabb48920cf6e39
athiththan11 <code19111995@gmail.com>
2019-08-12 00:55:38 +0530

    apim datasource added

--- src/services/datasources/generic.js
+++ src/services/datasources/generic.js
@@ -4,14 +4,16 @@ const path = require('path');
 const prettify = require('prettify-xml');
 const { cli } = require('cli-ux');
 
+const { logger } = require('../../utils/logger');
 const { parseXML, removeDeclaration } = require('../../utils/utility');
 
 let _p = process.cwd();
 let pMasterDatasource = '/repository/conf/datasources/master-datasources.xml';
 let pIdentity = '/repository/conf/identity/identity.xml';
 
-let _ = 'Generic';
-let _carbon = 'jdbc/WSO2' + _ + 'CarbonDB';
+let _ = 'GenericCarbonDB';
+let _name = 'Generic_Carbon_DB';
+let _carbon = 'jdbc/WSO2' + _;
 let _comment = 'HYDROGENERATED:';
 let _connectionUrl = '{specify connection url}';
 let _defaultAutoCommit = 'false';
@@ -28,14 +30,15 @@ let _utf8 = 'utf8';
 let _validationInterval = '30000';
 let _validationQuery = 'SELECT 1';
 
-exports.configureDatasource = async function (ocli, args) {
+exports.configureDatasource = async function (ocli, args, product) {
 	// variable set
 	_ = args._ ? args._ : _;
 
-	_carbon = 'jdbc/WSO2' + _ + 'CarbonDB';
+	_carbon = 'jdbc/WSO2' + _;
 	_description = _.toLowerCase() + ' datasource added';
 	_username = _.toLowerCase();
 
+	_name = args._name ? args._name : _name;
 	_carbon = args._carbon ? args._carbon : _carbon;
 	_comment = args._comment ? args._comment : _comment;
 	_connectionUrl = args._connectionUrl ? args._connectionUrl : _connectionUrl;
@@ -52,23 +55,108 @@ exports.configureDatasource = async function (ocli, args) {
 	_validationInterval = args._validationInterval ? args._validationInterval : _validationInterval;
 	_validationQuery = args._validationQuery ? args._validationQuery : _validationQuery;
 
-	cli.action.start('\taltering master-datasources.xml');
-	alterMasterDatasource(ocli).then(() => {
-		cli.action.stop();
-	}).then(() => {
-		cli.action.start('\taltering identity.xml');
-		alterIdentity(ocli);
-	}).then(() => {
-		cli.action.stop();
-	});
+	if (product === 'is') {
+		cli.action.start('\taltering master-datasources.xml');
+		alterMasterDatasource(ocli).then(() => {
+			cli.action.stop();
+		}).then(() => {
+			cli.action.start('\taltering identity.xml');
+			alterIdentity(ocli);
+		}).then(() => {
+			cli.action.stop();
+		});
+	} else if (product === 'am') {
+		cli.action.start('\taltering master-datasources.xml');
+		alterAMMasterDatasource(ocli).then(() => {
+			cli.action.stop();
+		});
+	}
 };
+
+// #region apim datasource implementations
+
+async function alterAMMasterDatasource(ocli) {
+	await parseXML(null, path.join(_p, pMasterDatasource)).then(master => {
+		let doc = new libxmljs.Document(master);
+		let genericElement = buildAMDatasource(doc);
+
+		let amElement = master.root()
+			.get('//*[local-name()="datasources"]/*[local-name()="datasource"][name="WSO2AM_DB"]');
+
+		let commentElement = new libxmljs.Comment(doc, amElement.toString());
+
+		master.root()
+			.get('//*[local-name()="datasources"]/*[local-name()="datasource"][name="WSO2AM_DB"]')
+			.addNextSibling(genericElement);
+
+		master.root()
+			.get('//*[local-name()="datasources"]/*[local-name()="datasource"][name="WSO2AM_DB"][1]')
+			.remove();
+
+		master.root()
+			.get('//*[local-name()="datasources"]/*[local-name()="datasource"][name="WSO2AM_DB"]')
+			.addPrevSibling(commentElement);
+
+		let altered = removeDeclaration(master.toString());
+
+		let _altered = altered.substring(0, altered.indexOf('<datasource><name>WSO2AM_DB</name>')) +
+			`${_n}<!-- ${_comment} datasource added -->\n` +
+			prettify(altered.substring(altered.indexOf('<datasource><name>WSO2AM_DB</name>'), altered.indexOf('</definition></datasource>') + '</definition></datasource>'.length), { indent: 4 }) +
+			altered.substring(altered.indexOf('</definition></datasource>') + '</definition></datasource>'.length, altered.length);
+
+		fs.writeFileSync(path.join(_p, pMasterDatasource), _altered, _utf8);
+	});
+}
+
+function buildAMDatasource(doc) {
+	let genericElement = new libxmljs.Element(doc, 'datasource');
+	genericElement
+		.node('name', 'WSO2AM_DB')
+		.parent()
+		.node('description', 'The datasource used for API Manager database')
+		.parent()
+		.node('jndiConfig')
+		.node('name', 'WSO2AM_DB')
+		.parent()
+		.parent()
+		.node('definition')
+		.attr({ type: 'RDBMS' })
+		.node('configuration')
+		.node('url', _connectionUrl)
+		.parent()
+		.node('username', _username)
+		.parent()
+		.node('password', 'hydrogen')
+		.parent()
+		.node('driverClassName', _driver)
+		.parent()
+		.node('maxActive', _maxActive)
+		.parent()
+		.node('maxWait', _maxWait)
+		.parent()
+		.node('minIdle', _minIdle)
+		.parent()
+		.node('testOnBorrow', _testOnBorrow)
+		.parent()
+		.node('validationQuery', _validationQuery)
+		.parent()
+		.node('validationInterval', _validationInterval)
+		.parent()
+		.node('defaultAutoCommit', _defaultAutoCommit);
+
+	return genericElement;
+}
+
+// #endregion
+
+// #region is datasource implementation
 
 // #region master-datasource parser
 
 async function alterMasterDatasource(ocli) {
 	await parseXML(null, path.join(_p, pMasterDatasource)).then(master => {
 		let doc = new libxmljs.Document(master);
-		let elem = '<datasource><name>WSO2_' + _.toUpperCase();
+		let elem = '<datasource><name>WSO2_' + _name.toUpperCase();
 
 		let genericElement = buildGenericDatasource(doc);
 
@@ -88,18 +176,18 @@ async function alterMasterDatasource(ocli) {
 		arr.shift();
 
 		let _altered = altered.substring(0, altered.lastIndexOf(elem)) +
-			`${_n}${_t}<!-- ${_comment} ${_description} -->\n${_t}` +
+			`${_n}<!-- ${_comment} ${_description} -->\n` +
 			prettify(generic, { indent: 4 }) + '\n' +
 			arr.join('\n');
 
-		fs.writeFileSync(path.join(_p, pMasterDatasource), prettify(_altered, { indent: 4 }) + '\n', _utf8);
+		fs.writeFileSync(path.join(_p, pMasterDatasource), _altered, _utf8);
 	});
 }
 
 function buildGenericDatasource(doc) {
 	let genericElement = new libxmljs.Element(doc, 'datasource');
 	genericElement
-		.node('name', `WSO2_${_.toUpperCase()}_CARBON_DB`)
+		.node('name', `WSO2_${_name.toUpperCase()}`)
 		.parent()
 		.node(
 			'description',
@@ -161,10 +249,9 @@ async function alterIdentity(ocli) {
 		// extract generic config
 		let _altered = altered.substring(0, altered.lastIndexOf(elem)) +
 			`\n${_t}\t<!-- ${_comment} ${_description}. changed jdbc/WSO2CarbonDB -->\n${_t}\t` +
-			altered.substring(altered.lastIndexOf(elem)) +
-			'\n\n';
+			altered.substring(altered.lastIndexOf(elem));
 
-		fs.writeFileSync(path.join(_p, pIdentity), prettify(_altered, { indent: 4 }) + '\n', _utf8);
+		fs.writeFileSync(path.join(_p, pIdentity), _altered, _utf8);
 	});
 }
 
