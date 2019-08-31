95a7b20f6b044edc3984ae1e7addab7d8a3fd951
Athiththan <code19111995@gmail.com>
2019-08-31 11:23:40 +0530

    logger enabled

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line node/no-unpublished-require
+const path = require('path');
 const winston = require('winston');
-const { createLogger, format } = winston;
+const { createLogger, format, transports } = winston;
 
 const logFormat = format.printf(({ level, message, timestamp }) => {
 	return `${timestamp} ${level}: ${message}`;
@@ -9,11 +10,8 @@ const logFormat = format.printf(({ level, message, timestamp }) => {
 const logger = createLogger({
 	format: format.combine(format.colorize(), format.timestamp(), format.align(), logFormat),
 	transports: [
-		// new transports.File({ filename: '/Users/athiththan/Athiththan/Projects/GitHub/hydrogen/logs/error.log', level: 'error' }),
-		// new transports.File({ filename: '/Users/athiththan/Athiththan/Projects/GitHub/hydrogen/logs/hydrogen.log' }),
-		new winston.transports.Console({
-			format: format.combine(format.timestamp(), logFormat),
-		}),
+		new transports.File({ filename: path.join(__dirname, '/logs', '/hydrogen-error.log'), level: 'error' }),
+		new transports.File({ filename: path.join(__dirname, '/logs', '/hydrogen.log') }),
 	],
 });
 
